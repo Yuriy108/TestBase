8e3254fe4a815a3105b6f57558b1c888e87f8859
Yuriy108 <sef.yuriy@gmail.com>
2021-05-24 18:32:48 +0300

    One

--- app/src/main/java/com/example/testbase/MainActivity.java
+++ app/src/main/java/com/example/testbase/MainActivity.java
@@ -4,26 +4,54 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     EditText email;
     EditText pass;
+    String TAG="DB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 2015);
+        db.collection("users")
+                .add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
         email=findViewById(R.id.editTextTextName);
         pass=findViewById(R.id.editTextTextPass);
         mAuth = FirebaseAuth.getInstance();
@@ -51,10 +79,10 @@ public class MainActivity extends AppCompatActivity {
         mAuth.createUserWithEmailAndPassword(email.getText().toString(),pass.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isComplete()){
+                if(task.isSuccessful()){
                     Toast.makeText(MainActivity.this,"New user is created",Toast.LENGTH_LONG).show();
                 }else {
-                    Toast.makeText(MainActivity.this,"Process down",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this,"Process down"+task.getException(),Toast.LENGTH_LONG).show();
                 }
 
 
